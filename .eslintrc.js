2e0a69c799b02174560d1af176b2344d41178583
LucasBargas <lucas.bargasoficial@gmail.com>
2022-08-31 20:18:19 -0300

    Configuring project

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'off',
+    'no-unused-vars': 'off',
+  },
+};
---
+++ db/conn.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('toughts', 'root', process.env.PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+try {
+  sequelize.authenticate(); // Checando conexão com o banco
+  console.log('Conectamos com sucesso com o Sequelize! Acesse: http://localhost:3000');
+} catch (error) {
+  console.log('Não foi possível conectar: ', error);
+}
+
+module.exports = sequelize;
