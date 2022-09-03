61f790d426ce42fecb986b0bf721caf5dbd2c192
LucasBargas <lucas.bargasoficial@gmail.com>
2022-09-02 21:13:34 -0300

    User registration phase

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -27,6 +27,50 @@ module.exports = class AuthController {
       return;
     }
 
-    res.redirect('/');
+    // Check if user exists by email
+    const checkIfUserExists = await User.findOne({ where: { email } });
+    if (checkIfUserExists) {
+      req.flash(
+        'message' /* key */,
+        'Este email já foi registrado, tente um diferente!' /* value */,
+      );
+
+      res.render('auth/register', { name });
+      return;
+    }
+
+    // Create a password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = { name, email, password: hashedPassword };
+
+    try {
+      // Adding user in data base
+      const createdUser = await User.create(user);
+
+      // Initialize session
+      req.session.userId = createdUser.id;
+
+      req.flash(
+        'message' /* key */,
+        'Cadastro realizado com sucesso!' /* value */,
+      );
+
+      console.log(createdUser);
+
+      req.session.save(() => {
+        res.redirect('/');
+      });
+    } catch (err) {
+      console.log(`An error: ${err}`);
+
+      req.flash(
+        'message' /* key */,
+        'Ocorreu um erro, tente novamente mais tarde!' /* value */,
+      );
+
+      res.render('auth/register');
+    }
   }
 };
