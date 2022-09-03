ca3567f59671c1b4d59ca601ad071b717aeff970
LucasBargas <lucas.bargasoficial@gmail.com>
2022-09-03 15:33:44 -0300

    Dashboard for toughts add - developed

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -7,6 +7,45 @@ module.exports = class ToughtController {
   }
 
   static async dashboard(req, res) {
-    res.render('toughts/dashboard');
+    const { userId } = req.session;
+    const user = await User.findOne({
+      where: { id: userId },
+      include: Tought,
+      plain: true,
+    });
+
+    // Check if user exists
+    if (!user) {
+      res.redirect('/login');
+    }
+
+    const toughts = user.Toughts.map((result) => result.dataValues);
+    res.render('toughts/dashboard', { toughts: toughts.reverse() });
+  }
+
+  static createTought(req, res) {
+    res.render('toughts/create');
+  }
+
+  static async createToughtSave(req, res) {
+    const tought = {
+      title: req.body.title,
+      UserId: req.session.userId,
+    };
+
+    try {
+      await Tought.create(tought);
+      req.flash('message', 'Pensamento criado com sucesso!');
+      req.session.save(() => {
+        res.redirect('/toughts/dashboard');
+      });
+    } catch (err) {
+      console.log(`An error: ${err}`);
+      req.flash(
+        'message' /* key */,
+        'Ocorreu um erro, tente novamente mais tarde!' /* value */,
+      );
+      res.render('toughts/create');
+    }
   }
 };
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -9,6 +9,12 @@ const { checkAuth } = require('../helpers/auth');
 
 // This dashboard route is protected with the middleware checkAuth
 // Your access only allowed if the user is authenticated
+router.get('/dashboard/adicionar', checkAuth, ToughtController.createTought);
+router.post(
+  '/dashboard/adicionar',
+  checkAuth,
+  ToughtController.createToughtSave,
+);
 router.get('/dashboard', checkAuth, ToughtController.dashboard);
 router.get('/', ToughtController.showToughts);
 
